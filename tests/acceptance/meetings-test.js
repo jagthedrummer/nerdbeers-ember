e53d0fc3f04b70e13016669ac0b9e841f387f3dc
Jeremy Green <jeremy@octolabs.com>
2015-06-27 12:53:01 -0500

    Write a test for creating pairings

--- tests/acceptance/meetings-test.js
+++ tests/acceptance/meetings-test.js
@@ -22,6 +22,7 @@ module('Acceptance: Meeting', {
     };
     defineFixturesFor('meeting', []);
     defineFixturesFor('location', []);
+    defineFixturesFor('pairing', []);
   },
   afterEach: function() {
     Ember.run(application, 'destroy');
@@ -69,6 +70,46 @@ test('create a new meeting', function(assert) {
     assert.equal(find('table tbody tr').length, 1);
   });
 });
+
+
+test('create a new meeting with a pairing', function(assert) {
+  visit('/meetings');
+  click('a:contains(New Meeting)');
+
+  andThen(function() {
+    assert.equal(currentPath(), 'meetings.new');
+
+    fillIn('label:contains(Date) input', new Date());
+    //fillIn('label:contains(Location) input', 'MyString');
+  });
+
+  click('a:contains(Add a pairing)');
+
+  andThen(function() {
+    fillIn('label:contains(Beer) input', 'f5');
+    fillIn('label:contains(Topic) input', 'f5 is great');
+    //fillIn('label:contains(Location) input', 'MyString');
+  });
+
+
+  andThen(function() {
+
+    click('input:submit');
+  });
+
+  andThen(function() {
+    assert.equal(find('#blankslate').length, 0);
+    assert.equal(find('table tbody tr').length, 1);
+  });
+
+  click('a:contains(Show)');
+
+  andThen(function() {
+    assert.equal(find('ol.pairings li').length, 1);
+  });
+
+});
+
 
 test('update an existing meeting', function(assert) {
   defineFixturesFor('meeting', [{ id: 1, date: new Date(), location: 1}]);
