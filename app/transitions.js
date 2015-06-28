9b2680c6cbd8ee26c18fe25a87d4c87197cfcb43
Jeremy Green <jeremy@octolabs.com>
2015-06-28 10:42:01 -0500

    Add liquid fire

---
+++ app/transitions.js
@@ -0,0 +1,14 @@
+export default function(){
+  this.transition(
+    this.fromRoute('index'),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+  this.transition(
+    this.toRoute(['locations.index','meetings.index','pairings.index']),
+    this.use('toLeft'),
+    this.reverse('toRight')
+  );
+
+};
